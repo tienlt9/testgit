41d40ad1d3ad78e6a6e4f9777ea2cc459fff59d8
tienteocri <tienlt777@gmail.com>
2022-07-13 23:38:58 +0700

    first commit

---
+++ Code/vd.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    cout << t;
+
+}
